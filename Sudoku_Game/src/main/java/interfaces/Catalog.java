d39d476800fe684ee9a946fa11ff993d4f8431ad
Adham-Shaaban <adham.mahmoud.shaaban@gmail.com>
2025-12-24 00:37:55 +0200

    adham solver veiwable interface and exception pCKAFE

--- Sudoku_Game/src/main/java/interfaces/Catalog.java
+++ Sudoku_Game/src/main/java/interfaces/Catalog.java
@@ -9,11 +9,6 @@ package interfaces;
  * Used to drive GUI startup flow.
  */
 public class Catalog {
-    public boolean current;
-    public boolean allModesExist;
-
-    public Catalog(boolean current, boolean allModesExist) {
-        this.current = current;
-        this.allModesExist = allModesExist;
-    }
+    
+    
 }
--- Sudoku_Game/src/main/java/logic/PermutationIterator.java
+++ Sudoku_Game/src/main/java/logic/PermutationIterator.java
@@ -6,25 +6,5 @@ package logic;
 
 public class PermutationIterator {
     
-    private final int[] cur = {1,1,1,1,1};
-    private boolean hasNext = true;
-
-    public boolean hasNext() { return hasNext; }
-
-    public int[] next() {
-        int[] out = cur.clone();
-        inc();
-        return out;
-    }
-
-    private void inc() {
-        for (int i = 4; i >= 0; i--) {
-            if (cur[i] < 9) {
-                cur[i]++;
-                return;
-            }
-            cur[i] = 1;
-        }
-        hasNext = false;
-    }
+    
 }
